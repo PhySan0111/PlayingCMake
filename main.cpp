72b76262ca7daec182cc80e8b6e02ea459b21433
PhySan <miguelsanches.sjm@gmail.com>
2023-03-11 12:04:58 +0000

    cmake: Adding a version number and a Configured header file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <concepts>
 
+#include "MainConfig.h"
 
 template <typename T>
 T adder(T a, T b) requires std::is_arithmetic_v<T> && (!std::same_as<T, char>)
@@ -8,9 +9,16 @@ T adder(T a, T b) requires std::is_arithmetic_v<T> && (!std::same_as<T, char>)
     return a + b;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    auto res = adder(1.3, 3.0);
-    std::cout << res << "\n";
-    return 0;
+  if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << MyMain_VERSION_MAJOR << "."
+              << MyMain_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+  }
+
+  auto res = adder(1.3, 3.0);
+  std::cout << res << "\n";
+  return 0;
 }
