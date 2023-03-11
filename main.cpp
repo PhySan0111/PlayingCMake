7662e530765a0f9e302b18b80a107ef41b2ba8ff
PhySan <miguelsanches.sjm@gmail.com>
2023-03-11 11:23:00 +0000

    cmake: specifying the c++ standard

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <concepts>
+
+
+template <typename T>
+T adder(T a, T b) requires std::is_arithmetic_v<T> && (!std::same_as<T, char>)
+{
+    return a + b;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto res = adder(1.3, 3.0);
+    std::cout << res << "\n";
     return 0;
 }
